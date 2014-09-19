7336977cf7cf1dfbbd87fbf24470146169cb6cae
lsolesen <lars@intraface.dk>
2014-09-19 11:32:40 +0000

    Added styling for the VIH Plugins

--- config.rb
+++ config.rb
@@ -1,6 +1,7 @@
 # Require any additional compass plugins here.
 require "compass_twitter_bootstrap";
 require "compass_radix";
+require 'sassy-buttons'
 
 # Set this to the root of your project when deployed:
 http_path = "/"

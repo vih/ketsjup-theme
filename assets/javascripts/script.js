41ea55419578451757e068b56b040f384f3dcd6c
Justin Cornell <justin@bluehorndigital.com>
2015-03-29 17:07:24 -0500

    vih/vies-build#9 It is not possible to expand menu on mobile
    Added toggle functionality to carets in main menu

--- assets/javascripts/script.js
+++ assets/javascripts/script.js
@@ -6,19 +6,27 @@
     Drupal.behaviors.ketsjup = {
         attach: function(context, settings) {
 
+            // Make sure search always shows up below header.
             $(window).resize(function() {
                positionSearch();
             });
 
+            // Click handler for search icon on desktop.
             $('#desk-search-toggle').click(function(e) {
                 e.preventDefault();
                 positionSearch();
                $('.navbar-right.navbar-form').fadeToggle(200);
             });
 
+            // Override the main menu toggle on mobile because it appears to be broken in Radix.
             $('a[data-toggle="collapse"]').click(function() {
                 var target = $(this).data("target");
                 $(target).slideToggle();
+            });
+
+
+            $('#main-menu .caret').click(function() {
+                $(this).parent().toggleClass('open');
             });
 
             function positionSearch() {
